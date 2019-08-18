57c93c771b601274bd51579bd7d5fa5cae77bd07
HTY <1581332872@163.com>
2019-08-18 18:51:41 +0800

    add session

--- blog/app.js
+++ blog/app.js
@@ -2,6 +2,15 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
+
+//session 数据
+const SESSION_DATA = {}
+
 // 用于处理postData
 const getPostData = (req) => {
     const promise = new Promise((resolve, reject) => {
@@ -47,36 +56,59 @@ const serverHandle = (req, res) => {
             return
         }
         const arr = item.split('=')
-        const key = arr[0]
-        const val = arr[1]
+
+        // trim方法，用于删除字符串首尾的空格
+        const key = arr[0].trim()
+        const val = arr[1].trim()
         req.cookie[key] = val
+
+        console.log(key, val)
     })
+
+    // 解析session
+    let userId = req.cookie.userId
+    let needSetCookie = false
+    if (userId) {
+        if (!SESSION_DATA[userId]) {
+            SESSION_DATA[userId] = {}
+        }
+
+    } else {
+        needSetCookie = true
+        userId = `${Date.now()}_${Math.random()}`
+        SESSION_DATA[userId] = {}
+    }
+    req.session = SESSION_DATA[userId]
+
 
     // getPostData函数的应用
     getPostData(req).then(postData => {
         req.body = postData
 
         // 处理blog路由
-        const blogData = handleBlogRouter(req, res)
-        if (blogData) {
-            res.end(
-                JSON.stringify(blogData)
-            )
+        const blogResult = handleBlogRouter(req, res)
+        if (blogResult) {
+            blogResult.then(blogData => {
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie', `userId=${userId};path=/;httpOnly;expires=${getCookieExpires()}`)
+
+                }
+                res.end(
+                    JSON.stringify(blogData)
+                )
+            })
+
             return
         }
-        // 处理user路由
-        // const userData = handleUserRouter(req, res)
-        // if (userData) {
-        //     res.end(
-        //         JSON.stringify(userData)
-        //     )
-        //     return
-        // }
+
 
         // 处理user的路由
         const userResult = handleUserRouter(req, res)
         if (userResult) {
             userResult.then(userData => {
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie', `userId=${userId};path=/;httpOnly;expires=${getCookieExpires()}`)
+                }
                 res.end(
                     JSON.stringify(userData)
                 )
