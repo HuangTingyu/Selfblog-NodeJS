5bafad3136b5019d04313836310715c74749fc9c
sakurahuang <sakurahuang@tencent.com>
2021-03-18 20:55:21 +0800

    环境初步

--- express-blog/routes/blog.js
+++ express-blog/routes/blog.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var request = require('request')
 
 router.get('/list',function(req,res,next){
     res.json({
@@ -9,10 +10,15 @@ router.get('/list',function(req,res,next){
 })
 
 router.get('/detail',function(req,res,next){
-    res.json({
-        errno:0,
-        data:'detail'
+    request.get('http://wwitil.oa.com/datacenter/cgi-bin/wework_mng_newtapd.py?cmd=6004&itid=1010121621001204723', function(error, response, body){
+        res.json({response})
+        // console.log(response && response.statusCode)
+        // console.log(response)
     })
+    // res.json({
+    //     errno:0,
+    //     data:'detail'
+    // })
 })
 
 module.exports = router
