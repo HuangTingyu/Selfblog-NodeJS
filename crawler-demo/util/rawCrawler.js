32646c86ad56105a4d96dd737301d5494a525e25
sakurahuang <sakurahuang@tencent.com>
2021-03-22 15:02:34 +0800

    爬虫初步

--- crawler-demo/util/rawCrawler.js
+++ crawler-demo/util/rawCrawler.js
@@ -40,14 +40,42 @@ async function getLink(){
     await page.goto(link)
     
     // console.log(page)
-    const aTags = await page.$$eval('a', aTags => {
+    const aTags = await page.$$eval('a', ele => {
         let linkArr = []
-        aTags.forEach(aItem => {
-            linkArr.push(aItem.href)
+        ele.forEach(item => {
+            item && item.href && linkArr.push(item.href)
         })
         return linkArr.join(',')
     })
-    fs.writeFile("../assets/writeCsv.csv", aTags, (err) => {
+    const imgs = await page.$$eval('img', ele => {
+        let linkArr = []
+        ele.forEach(item => {
+            item && item.src && linkArr.push(item.src)
+        })
+        return linkArr.join(',')
+    })
+    const scripts = await page.$$eval('script', ele => {
+        let linkArr = []
+        ele.forEach(item => {
+            item && item.src && linkArr.push(item.src)
+        })
+        return linkArr.join(',')
+    })
+    const links = await page.$$eval('link', ele => {
+        let linkArr = []
+        ele.forEach(item => {
+            item && item.href && linkArr.push(item.href)
+        })
+        return linkArr.join(',')
+    })
+    let linkArr = []
+    linkArr.push(aTags)
+    linkArr.push(imgs)
+    linkArr.push(scripts)
+    linkArr.push(links)
+
+    
+    fs.writeFile("../assets/writeCsv.csv", linkArr.join(','), (err) => {
         console.log(err || "done")
     })
 }
---
+++ crawler-demo/util/test.js
@@ -0,0 +1,9 @@
+let a = 'https://www.baidu.com,https://www.zhihu.com,'
+let b = 'https://www.bilibili.com'
+let c = 'https://www.google.com'
+
+let linkArr = []
+linkArr.push(a)
+linkArr.push(b)
+linkArr.push(c)
+console.log(linkArr.join(','))
